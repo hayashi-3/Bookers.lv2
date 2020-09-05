11463a9eb0c59ff2fc76bca365d76d58b409e9c9
vagrant <vagrant@localhost.localdomain>
2020-09-05 23:02:30 +0000

    work3-9

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,12 @@
 class BookCommentsController < ApplicationController
+	before_action :authenticate_user!
 
 	def create
 		@book = Book.find(params[:book_id])
 		@book_comment = current_user.book_comments.new(book_comment_params)
 		@book_comment.book_id = @book.id
 	 if @book_comment.save
+	 	flash[:success] = "Comment was successfully created."
 	    redirect_back(fallback_location: book_path(@book))
 	 else
 		redirect_to book_path(@book)
